28866aeb54f348bc695b2448fa3c06d820d3590d
Taku Kobayashi <keep_slimbody@yahoo.co.jp>
2019-06-16 23:50:01 +0900

    pureを試してみた

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import purecss from 'purecss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <form class="pure-form">
+          <input type="text" name="q" />
+          <input type="submit" value="作成する" class="pure-button pure-button-primary" />
+        </form>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
