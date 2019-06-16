e3d92fc03a9f61af3086799a78d1f3a7e6d776c5
Taku Kobayashi <keep_slimbody@yahoo.co.jp>
2019-06-17 04:42:18 +0900

    とりあえずAPIへのRequestを送ることができた。URLも反映できた

--- frontend/src/compoments/generate-slide-form.js
+++ frontend/src/compoments/generate-slide-form.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import purecss from 'purecss';
+import axios from 'axios';
 
 export default class GenerateSlideForm extends React.Component{
   constructor(props) {
     super(props);
+
+    this.generateSlideSubmit = this.generateSlideSubmit.bind(this);
+  }
+
+  generateSlideSubmit(event){
+    console.log(process.env.REACT_APP_API_ROOT_URL);
+    axios.get(process.env.REACT_APP_API_ROOT_URL).then(res => {
+      console.log(res);
+    })
+    event.preventDefault();
   }
 
   render(){
     return (
-      <form className="pure-form">
+      <form onSubmit={this.generateSlideSubmit} className="pure-form">
         <input type="text" name="q" />
         <input type="submit" value="作成する" className="pure-button pure-button-primary" />
       </form>
