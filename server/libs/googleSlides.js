be0e326183a94ba076fea4aa2fc1b268787b85d0
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-06-18 20:09:30 +0900

    なんかおかしいけど大体こんな感じ?

--- server/libs/googleSlides.js
+++ server/libs/googleSlides.js
@@ -10,5 +10,13 @@ const createSlides = async function createSlides(credentials) {
   let sl = await slide.presentations.create({
     title: "slide_name"
   })
+  let slides = await slide.presentations.batchUpdate({
+    presentationId: sl.data.presentationId,
+    requests: [{
+      createImage: {
+        url: "https://hashibaminone.com/wp-content/uploads/2018/08/LINE%E3%82%BF%E3%82%A4%E3%83%A0%E3%83%A9%E3%82%A4%E3%83%B3%E3%81%AE%E6%9C%80%E9%81%A9%E3%81%AA%E7%94%BB%E5%83%8F%E3%82%B5%E3%82%A4%E3%82%B9%E3%82%99.jpg"
+      }
+    }]
+  })
   return sl;
 };
