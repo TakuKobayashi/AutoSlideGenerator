a2e85c07dd8df382de5f9c0d713eb042f35ceb22
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-06-18 17:31:58 +0900

    プルダウンを試しに入れてみた

--- frontend/src/compoments/generate-slide-form.js
+++ frontend/src/compoments/generate-slide-form.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import purecss from 'purecss';
 import axios from 'axios';
+import {Collapse} from 'react-collapse';
 
 export default class GenerateSlideForm extends React.Component{
   constructor(props) {
@@ -24,6 +25,9 @@ export default class GenerateSlideForm extends React.Component{
     return (
       <form onSubmit={this.generateSlideSubmit} className="pure-form">
         <input type="text" name="q" />
+        <Collapse isOpened={true || false}>
+          <div>Random content</div>
+        </Collapse>
         <input type="submit" value="作成する" className="pure-button pure-button-primary" />
       </form>
     );
