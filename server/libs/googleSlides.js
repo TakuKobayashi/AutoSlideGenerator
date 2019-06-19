a43c5251aced6bf1646b4414141e4ff95286cfff
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-06-19 15:27:19 +0900

    Google SlideにSlideを作成することができました

--- server/libs/googleSlides.js
+++ server/libs/googleSlides.js
@@ -1,22 +1,43 @@
 const { google } = require('googleapis');
+const uuid = require('uuid/v4');
 
-const createSlides = async function createSlides(credentials) {
+exports.createPresentationAndSlides = async function createPresentationAndSlides(credentials, presentationProperty = {}, resourceObjects = []) {
   const oauth2Client = new google.auth.OAuth2(process.env.GOOGLE_OAUTH_CLIENT_ID, process.env.GOOGLE_OAUTH_CLIENT_SECRET);
   oauth2Client.setCredentials(credentials);
-  const slide = google.slides({
-    version: "v1",
-    auth: oauth2Client,
-  })
-  let sl = await slide.presentations.create({
-    title: "slide_name"
-  })
-  let slides = await slide.presentations.batchUpdate({
-    presentationId: sl.data.presentationId,
-    requests: [{
-      createImage: {
-        url: "https://hashibaminone.com/wp-content/uploads/2018/08/LINE%E3%82%BF%E3%82%A4%E3%83%A0%E3%83%A9%E3%82%A4%E3%83%B3%E3%81%AE%E6%9C%80%E9%81%A9%E3%81%AA%E7%94%BB%E5%83%8F%E3%82%B5%E3%82%A4%E3%82%B9%E3%82%99.jpg"
-      }
-    }]
-  })
-  return sl;
+  const googleSlides = google.slides({version: "v1", auth: oauth2Client})
+  const newPresentationResponse = await googleSlides.presentations.create(presentationProperty)
+  // newPresentationResponse.data.pageSize でPresentationの幅、高さの情報が取れる
+  const slideImageObjects = createImageSlideObjects(resourceObjects);
+  const slides = await googleSlides.presentations.batchUpdate({
+    presentationId: newPresentationResponse.data.presentationId,
+    resource: {
+      requests: slideImageObjects,
+    }
+  });
+  const updatePresentationResponse = await googleSlides.presentations.get(newPresentationResponse.data.presentationId);
+  return updatePresentationResponse.data;
 };
+
+function createImageSlideObjects(resourceObjects = []) {
+  const objects = [];
+  for (const resourceObject of resourceObjects){
+    const slideObjectId = uuid();
+    objects.push({
+      createSlide: {
+        objectId: slideObjectId,
+      },
+    });
+    // TODO: 真ん中にしてあげるのが親切かも
+    const imageObjectId = uuid();
+    objects.push({
+      createImage: {
+        objectId: imageObjectId,
+        url: resourceObject.image_url,
+        elementProperties: {
+          pageObjectId: slideObjectId,
+        }
+      }
+    })
+    return objects;
+  }
+}
