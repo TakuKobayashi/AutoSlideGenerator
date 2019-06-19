9415b14e7e411395e12db87968a666b1b66d05a7
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-06-19 16:10:25 +0900

    lambdaで呼べるであろうものを作った

--- server/lambda/slide-generate/index.js
+++ server/lambda/slide-generate/index.js
@@ -1,9 +1,41 @@
+const requireRoot = require('app-root-path').require;
+const googleImageSearch = requireRoot('/libs/googleImageSearch');
+const frickrSearch = requireRoot('/libs/frickrSearch');
+const twitterStatus = requireRoot('/libs/twitterStatus');
+const googleSlides = requireRoot('/libs/googleSlides');
+
 exports.handler = async (event, context) => {
   console.log(event);
-  // TODO implement
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!'),
-  };
-  return response;
+  const googleAccessToken = event.googleAccessToken;
+  const searchWords = event.words.split(',');
+  const presentationProperty = JSON.parse(event.presentationProperty || '{}');
+  const imageResources = [];
+  for (const searchWord of searchWords) {
+    if (event.searchWebsiteType === 'frickr') {
+      const searchPhotos = await frickrSearch.searchFlickrPhotos({
+        text: searchWord,
+      });
+      const targetPhoto = searchPhotos.photo[Math.floor(Math.random() * searchPhotos.photo.length)];
+      imageResources.push(searchPhotos.convertToPhotoToObject(targetPhoto));
+    } else if (event.searchWebsiteType === 'twitter') {
+      const searchTweets = await twitterStatus.searchResourceTweets({
+        q: searchWord,
+      });
+      const searchResults = twitterStatus.convertStatusesToResourcesObject(searchTweets);
+      imageResources.push(searchResults.images[Math.floor(Math.random() * searchResults.images.length)]);
+    } else {
+      const searchResults = await googleImageSearch.searchGoogleToObjects({
+        q: searchWord,
+      });
+      imageResources.push(searchResults[Math.floor(Math.random() * searchResults.length)]);
+    }
+  }
+  const presentation = await googleSlides.createPresentationAndSlides(
+    {
+      access_token: googleAccessToken,
+    },
+    presentationProperty,
+    imageResources,
+  );
+  return presentation;
 };
