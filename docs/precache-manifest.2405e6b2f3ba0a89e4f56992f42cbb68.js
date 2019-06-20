51a7a4bc39002d667305df3f0307407d0d9db49b
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-06-20 12:16:27 +0900

    build new feature.

---
+++ docs/precache-manifest.2405e6b2f3ba0a89e4f56992f42cbb68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f08446d7a8f972036a1b62efdf65e9a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d596909299d24165693b",
+    "url": "./static/css/2.7ade3b48.chunk.css"
+  },
+  {
+    "revision": "e5f4f67f14420bf353f8",
+    "url": "./static/css/main.9315ce62.chunk.css"
+  },
+  {
+    "revision": "d596909299d24165693b",
+    "url": "./static/js/2.e96cc05d.chunk.js"
+  },
+  {
+    "revision": "e5f4f67f14420bf353f8",
+    "url": "./static/js/main.6d2c8f33.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "f86f89da7c9a4cdc9242a7f20f873fd7",
+    "url": "./static/media/AutoSlideGeneratorLogo.f86f89da.png"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.50334aa8b861da405894c96467a72990.js"
+  "./precache-manifest.2405e6b2f3ba0a89e4f56992f42cbb68.js"
 );
 
 self.addEventListener('message', (event) => {
