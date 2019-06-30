06f9186110a0ec567752c8ec34b5e08f7a015fa9
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-06-30 16:40:14 +0900

    機能を追加

--- frontend/src/compoments/generate-slide-form.js
+++ frontend/src/compoments/generate-slide-form.js
@@ -2,6 +2,7 @@ import React from 'react';
 import purecss from 'purecss';
 import axios from 'axios';
 import {Collapse} from 'react-collapse';
+import Push from 'push.js';
 
 export default class GenerateSlideForm extends React.Component{
   constructor(props) {
