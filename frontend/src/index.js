b05b9b045c9e95cee3b4d445176f776eceb7a0a0
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-07-17 01:49:47 +0900

    redux-persistでreduxのstoreの情報を永続的に保存する

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,17 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { persistReducer, persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
+import storage from 'redux-persist/lib/storage'
 import './index.css';
 import App from './container/app'
 import * as serviceWorker from './serviceWorker';
 
 import reducer from './reducer';
 
-const store = createStore(reducer);
+// 永続化の設定
+const persistConfig = {
+  key: 'root', // Storageに保存されるキー名を指定する
+  storage, // 保存先としてlocalStorageがここで設定される
+  //whitelist: ['todos'] // Stateは`todos`のみStorageに保存する
+  // blacklist: ['visibilityFilter'] // `visibilityFilter`は保存しない
+}
+
+// 永続化設定されたReducerとして定義
+const persistedReducer = persistReducer(persistConfig, reducer)
+
+const store = createStore(persistedReducer);
+const persistor = persistStore(store);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root'),
 );
