2ac1eb1cec044931762ba832aced72ffaf25c7e3
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-07-17 20:00:30 +0900

    とりあえずフロントにそれらしきものを入れる

--- frontend/src/compoments/generate-slide-form.js
+++ frontend/src/compoments/generate-slide-form.js
@@ -39,9 +39,21 @@ export default class GenerateSlideForm extends React.Component {
     return (
       <form onSubmit={this.generateSlideSubmit} className="pure-form">
         <div>{JSON.stringify(googleAccount)}</div>
-        <input type="text" name="q" />
+        <div className="pure-u-1">
+          <input type="text" name="q" placeholder="スライドにしたい画像のキーワードを,(カンマ区切り)で入力していってください" />
+        </div>
+        <h2>素材の収集元</h2>
         <Collapse isOpened={true || false}>
-          <div>Random content</div>
+          <h2>素材の収集元</h2>
+          <p><input type="radio" name="rd" id="rd1" /><label htmlFor="rd1">Google画像検索</label></p>
+          <p><input type="radio" name="rd" id="rd2" /><label htmlFor="rd2">Twitter</label></p>
+          <p><input type="radio" name="rd" id="rd3" /><label htmlFor="rd2">Flickr</label></p>
+          <p><input type="radio" name="rd" id="rd4" /><label htmlFor="rd2">Instagram</label></p>
+          <h2>プレゼンの出力先</h2>
+          <p><input type="radio" name="rd" id="rd11" /><label htmlFor="rd1">Google Slide</label></p>
+          <p><input type="radio" name="rd" id="rd12" /><label htmlFor="rd2">HTML</label></p>
+          <h2>プッシュ通知</h2>
+          <p><input type="checkbox" name="cb" id="cb1" /><label htmlFor="cb1">出来上がったらプッシュ通知でお知らせする</label></p>
         </Collapse>
         <input type="submit" value="作成する" className="pure-button pure-button-primary" />
       </form>
