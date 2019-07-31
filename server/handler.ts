583f43f9577c46abb56ad16110052a9c4c79d744
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-07-31 16:25:00 +0900

    POST受け取り用

--- server/handler.ts
+++ server/handler.ts
@@ -4,6 +4,9 @@ import 'source-map-support/register';
 export const hello: APIGatewayProxyHandler = async (event, _context) => {
   return {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin" : "*" // Required for CORS support to work
+    },
     body: JSON.stringify({
       message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
       input: event,
