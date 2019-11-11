54e82465a7147013aa4d5edbf76b34a336dfe0ae
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-11-12 05:24:50 +0900

    Login Button以外の実証実験

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,13 +5,27 @@ import backgroundVideoWebm from './Background.webm';
 import backgroundVideoJpg from './Background.jpg';
 import './App.css';
 import GenerateSlideForm from './compoments/generate-slide-form';
-import GoogleLoginButton from './compoments/google-login-button';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isSignedIn: false,
+    }
+  }
 
-    console.log(this.props);
+  componentDidMount() {
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client.init({
+        clientId: process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID, //"clientID"は個人で取得する
+        scope: "email"
+      }).then(() => {
+         const auth = window.gapi.auth2.getAuthInstance();
+         console.log(auth);
+
+         this.setState({ isSignedIn: auth.isSignedIn.get() });
+      });
+    });
   }
 
   render() {
@@ -21,7 +35,6 @@ class App extends React.Component {
           <source src={backgroundVideoWebm} type="video/webm" />
           <source src={backgroundVideoMP4} type="video/mp4" />
         </video>
-        <GoogleLoginButton {...this.props} />
         <img src={logo} className="App-logo" alt="logo" />
         <GenerateSlideForm {...this.props} />
       </div>
