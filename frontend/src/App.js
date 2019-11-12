53f3a4aefd45e659c522c1e70876a362f7f7ea9b
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-11-12 17:48:37 +0900

    なかなか難しいのでAppの中で完結するようにしてみる

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,20 +5,74 @@ import backgroundVideoMP4 from './Background.mp4';
 import backgroundVideoWebm from './Background.webm';
 import backgroundVideoJpg from './Background.jpg';
 import './App.css';
-import GenerateSlideForm from './compoments/generate-slide-form';
+import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCog } from '@fortawesome/free-solid-svg-icons';
+import { Card, Input, Accordion, AccordionSection, Select, Button, CheckboxToggle, Textarea } from 'react-rainbow-components';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      isSignedIn: false,
-    };
     this.responseGoogle = this.responseGoogle.bind(this);
+    this.state = {
+      words: [],
+      searchWebsiteType: 'google',
+      exportType: 'googleSlide',
+      pushEnable: false,
+      loading: false,
+      slideTitle: '',
+      googleAccount: {},
+    };
+
+    this.generateSlideSubmit = this.generateSlideSubmit.bind(this);
+    this.onSelectChanged = this.onSelectChanged.bind(this);
+    this.onPushChecked = this.onPushChecked.bind(this);
+  }
+
+  generateSlideSubmit(event) {
+    this.setState({ loading: true });
+    this.generateSlideRequest();
+    event.preventDefault();
+  }
+
+  onSelectChanged(event) {
+    console.log(event);
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  async generateSlideRequest() {
+    console.log(this.state);
+    const slideWords = this.state.words.join(',').split(',');
+    if (slideWords.length <= 0) {
+      return;
+    }
+    let slideTitle = this.state.slideTitle;
+    if (!slideTitle) {
+      slideTitle = slideWords[0];
+    }
+    const res = await axios.post(process.env.REACT_APP_API_ROOT_URL + '/slide/generate', {
+      words: slideWords.join(','),
+      searchWebsiteType: this.state.searchWebsiteType,
+      exportType: this.state.exportType,
+      pushEnable: this.state.pushEnable,
+      googleAccessToken: this.state.googleAccount.accessToken,
+      presentationProperty: {
+        title: slideTitle,
+      },
+    });
+    this.setState({ loading: false });
+    console.log(res);
+  }
+
+  onPushChecked(event, checked) {
+    console.log(event);
+    console.log(checked);
+    this.setState({ pushEnable: checked });
   }
 
   responseGoogle(response) {
     this.setState({
-      googleLogin: {
+      googleAccount: {
         profileObj: response.profileObj,
         tokenObj: response.tokenObj,
         accessToken: response.accessToken,
@@ -27,7 +81,6 @@ class App extends React.Component {
       },
     });
     console.log(this.state);
-    console.log(response);
   }
 
   render() {
@@ -52,7 +105,48 @@ class App extends React.Component {
             cookiePolicy={'single_host_origin'}
           />
         </p>
-        <GenerateSlideForm {...this.props} />
+        <div className="rainbow-p-vertical_large rainbow-p-horizontal_xx-large rainbow-m-horizontal_xx-large">
+          <Card>
+            <Textarea
+              placeholder="スライドにしたい画像のキーワードを,(カンマ区切り)で入力していってください"
+              onChange={(e) => this.setState({ words: [e.target.value] })}
+              rows={2}
+            />
+
+            <Accordion>
+              <AccordionSection icon={<FontAwesomeIcon icon={faCog} className="rainbow-color_brand" />} label="詳細設定">
+                <Select
+                  label="素材の収集元"
+                  options={[
+                    { value: 'google', label: 'Google画像検索' },
+                    { value: 'twitter', label: 'Twitter' },
+                    { value: 'instagram', label: 'Instagram' },
+                  ]}
+                  onChange={this.onSelectChanged}
+                />
+                <Input
+                  label="slide title"
+                  placeholder="スライド名を決めたい場合は入力してください"
+                  onChange={(e) => this.setState({ slideTitle: e.target.value })}
+                  type="text"
+                />
+                <Select
+                  label="Slideの出力先"
+                  options={[{ value: 'googleSlide', label: 'Google Slide' }, { value: 'html', label: 'HTML' }]}
+                  name="exportType"
+                  value={this.state.exportType}
+                  onChange={this.onSelectChanged}
+                />
+                <CheckboxToggle
+                  label="出来上がったらプッシュ通知でお知らせする"
+                  value={this.state.pushEnable}
+                  onChange={this.onPushChecked}
+                />
+              </AccordionSection>
+            </Accordion>
+            <Button label="作成する" onClick={this.generateSlideSubmit} variant="brand" isLoading={this.state.loading} />
+          </Card>
+        </div>
       </div>
     );
   }
